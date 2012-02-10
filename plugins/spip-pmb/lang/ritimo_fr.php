67968ee043cca44a8fe8241241e0a70b371825b9
Olivier Sarrat <olivier.sarrat@laposte.net>
2012-02-10 15:12:48 +0100

    Fichier de langue déplacé pour packaging (issue #6). Commit aussi lié à issue #2 car traite d'un fichier de traduction.

---
+++ plugins/spip-pmb/lang/ritimo_fr.php
@@ -0,0 +1,9 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'tous_les_centres' => 'Tous les centres Ritimo',
+		'y_compris' => 'y compris',
+		'documents_du_centre_de_documentation' => 'Documents du centre de documentation'
+	);
+
+?>
