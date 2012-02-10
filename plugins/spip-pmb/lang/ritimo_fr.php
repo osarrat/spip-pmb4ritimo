aeb77eeb4e39f1c3736c3051b557c2057d9b29d3
Olivier Sarrat <olivier.sarrat@laposte.net>
2012-02-10 20:07:50 +0100

    Lien vers OPAC pour recherche avancée (closes issue #11)

--- plugins/spip-pmb/lang/ritimo_fr.php
+++ plugins/spip-pmb/lang/ritimo_fr.php
@@ -3,7 +3,9 @@
 	$GLOBALS[$GLOBALS['idx_lang']] = array(
 		'tous_les_centres' => 'Tous les centres Ritimo',
 		'y_compris' => 'y compris',
-		'documents_du_centre_de_documentation' => 'Documents du centre de documentation'
+		'documents_du_centre_de_documentation' => 'Documents du centre de documentation',
+		'lien_recherche_avancee' => 'Recherche avanc&eacute;e, cliquez ici',
+		'recherche_bibliographique' => 'Recherche bibliographique'
 	);
 
 ?>
