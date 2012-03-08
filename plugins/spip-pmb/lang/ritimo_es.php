607f25bfb98779414f89e297220d9a304b6c5bdc
Thierry Eraud <t.eraud@ritimo.org>
2012-03-08 08:44:32 +0100

    mise en place des fichiers de langues pour l'anglais et l'espagnol

---
+++ plugins/spip-pmb/lang/ritimo_es.php
@@ -0,0 +1,11 @@
+<?php
+
+	$GLOBALS[$GLOBALS['idx_lang']] = array(
+		'tous_les_centres' => 'Todos los centros Ritimo',
+		'y_compris' => 'incluso',
+		'documents_du_centre_de_documentation' => 'Documentos del centro de documentación',
+		'lien_recherche_avancee' => 'Búsqueda avanzada, haga clic aquí',
+		'recherche_bibliographique' => 'Búsqueda bibliográfica'
+	);
+
+?>
